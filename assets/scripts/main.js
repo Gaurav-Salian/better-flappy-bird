091bdecbfe010dd8daabcbffe20e87c47adbed3f
Gaurav-Salian <gauravsalian11@gmail.com>
2024-08-27 20:09:03 +0530

    initial commit

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -10,7 +10,7 @@ class Game{
         this.player = new Player(this);
         this.sound = new AudioControl(this);
         this.obstacles =[];
-        this.numberOfObstacles =2;
+        this.numberOfObstacles =200;
         this.gravity;
         this.speed;
         this.minSpeed;
